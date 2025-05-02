f25405833d9193f34b6e430496b8bd51f7c874d3
FNU Shaliwa <sf452@cornell.edu>
2025-05-02 21:00:33 +0800

    Initial Commit for Frontend - Create react frontend project - Build basic ProductCard & display products

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { FaBeer } from 'react-icons/fa'
+import Products from './components/Products'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Products />
+    </div>
+  )
+}
+
+export default App
